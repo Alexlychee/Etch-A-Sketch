79cba90b5f0088193e27ad603979a42cbf6d452d
Alex Ly <aly26@csu.fullerton.edu>
2024-08-17 19:53:44 -0700

    added grid and hover

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const DEFAULT_N = 16;
-
-let grid = document.querySelector(".container");
 let n = DEFAULT_N;
-let columns;
-let rows;
+let grid = document.querySelector(".container");
 
-function createGrid() {
-    for (let i = 0; i <= n; i++) {
-        columns = document.createElement("div");
-        columns.classList.add("column");
+createGrid(n);
+
+function createGrid(n) {
+    grid.innerHTML = '';
+
+    let gridSize = grid.clientWidth / n;
+
+    for (let i = 0; i < n * n; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("grid-item");
+
+        cell.style.width = `${gridSize}px`;
+        cell.style.height = `${gridSize}px`;
+        grid.appendChild(cell);
     }
+
+    let squares = document.querySelectorAll(".grid-item");
+    squares.forEach(square => square.addEventListener("mouseover", event =>{
+        event.target.classList.add("color");
+    }));
 }
